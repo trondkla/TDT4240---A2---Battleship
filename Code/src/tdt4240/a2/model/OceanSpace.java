f88928c2cef87c8f80a914b5ec092f92dd31408b
Alexander Perry <yrrepxela@gmail.com>
2012-03-15 15:25:42 +0100

    Added more code :D

--- Code/src/tdt4240/a2/model/OceanSpace.java
+++ Code/src/tdt4240/a2/model/OceanSpace.java
@@ -1,19 +1,22 @@
 package tdt4240.a2.model;
 
 /**
- * Created by IntelliJ IDEA.
- * User: tk
- * Date: 15.03.12
- * Time: 11.27
- * To change this template use File | Settings | File Templates.
  */
 public class OceanSpace {
 
     private OceanSpaceSize oceanSpaceSize;
-    //private [][];
+    private OceanTile oceanSpace[][];
+    private Player player;
 
-    public OceanSpace(OceanSpaceSize oceanSpaceSize){
+
+    public OceanSpace(OceanSpaceSize oceanSpaceSize, Player player){
         this.oceanSpaceSize = oceanSpaceSize;
-        //.. = new [oceanSpaceSize.getSize()][oceanSpaceSize.getSize()];
+        this.player = player;
+        oceanSpace = new OceanTile[oceanSpaceSize.getSize()][oceanSpaceSize.getSize()];
+
+    }
+    
+    public OceanTile[][] getOceanSpace(){
+        return oceanSpace;
     }
 }
---
+++ Code/src/tdt4240/a2/model/Player.java
@@ -0,0 +1,17 @@
+package tdt4240.a2.model;
+
+import android.graphics.Color;
+
+/**
+ */
+public class Player {
+    
+    private String name;
+    private Color color;
+    //add more smart shit
+    
+    public Player(String name, Color color){
+        this.name = name;
+        this.color = color;
+    }
+}
