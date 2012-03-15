6d8792bc6b8b80d4c4c8fe1d7e9abfacbf9fb34e
Alexander Perry <yrrepxela@gmail.com>
2012-03-15 13:54:58 +0100

    Did as TK told me to

---
+++ Code/src/tdt4240/a2/model/OceanSpaceSize.java
@@ -0,0 +1,23 @@
+package tdt4240.a2.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tk
+ * Date: 15.03.12
+ * Time: 11.28
+ * To change this template use File | Settings | File Templates.
+ */
+public enum OceanSpaceSize {
+    SMALL(10),
+    MEDIUM(12),
+    LARGE(14);
+    
+    private int size;
+    private OceanSpaceSize(int size){
+        this.size = size;
+    }
+    
+    public int getSize(){
+        return size;
+    }
+}
---
+++ Code/src/tdt4240/a2/model/WarshipState.java
@@ -0,0 +1,11 @@
+package tdt4240.a2.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Perry
+ * Date: 15.03.12
+ * Time: 13:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class WarshipState {
+}
