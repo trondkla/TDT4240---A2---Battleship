d6952830ed5a455823f4416143d3c6cae26fae48
Alexander Perry <yrrepxela@gmail.com>
2012-03-15 11:49:44 +0100

    Added warship model

---
+++ Code/src/tdt4240/a2/model/Warship.java
@@ -0,0 +1,12 @@
+package tdt4240.a2.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Perry
+ * Date: 15.03.12
+ * Time: 11:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class Warship {
+
+}
